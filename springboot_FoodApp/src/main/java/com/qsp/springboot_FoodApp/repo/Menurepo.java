e90280c157c18a459841ae031770a67918f315c0
nandithapb23 <122531318+nandithapb23@users.noreply.github.com>
2023-10-07 07:09:46 +0530

    Delete springboot_FoodApp directory

--- springboot_FoodApp/src/main/java/com/qsp/springboot_FoodApp/repo/Menurepo.java
+++
@@ -1,9 +0,0 @@
-package com.qsp.springboot_FoodApp.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qsp.springboot_FoodApp.dto.Menu;
-
-public interface Menurepo extends JpaRepository<Menu, Integer>{
-
-}
--- springboot_FoodApp/src/main/java/com/qsp/springboot_FoodApp/repo/Productrepo.java
+++
@@ -1,9 +0,0 @@
-package com.qsp.springboot_FoodApp.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qsp.springboot_FoodApp.dto.Product;
-
-public interface Productrepo extends JpaRepository<Product, Integer>{
-
-}
